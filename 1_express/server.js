1f2e7ce83e8692f5feb7a6f22fae7b14f9886285
Letícia Gabrielle <leticiag.soares.dev@gmail.com>
2024-07-10 08:26:21 -0300

    conceito sobre rotas

--- 1_express/server.js
+++ 1_express/server.js
@@ -5,8 +5,28 @@ const express = require('express')
 const PORT = 3333
 const app = express()
 
+//middleware
+//Aceitar JSON no body
+app.use(express.json())
+
 //.get(rota, callback)
+/*Request HTTP
+* query params - ....:3333/pessoas?nome="Carlos"&idade=32
+* -- Rotas do tipo GET (filtros e buscas)
+
+* route params - ....:3333/pessoas/5
+* -- Rotas do tipo GET, PUT, PATCH, DELETE (listar um elemento)
+
+* body params - ....:3333/pessoas
+* -- Rotas do tipo POST (Cadastro de informações)
+*/
+
+//query params
 app.get("/users", (request, response)=>{
+    // const query = request.query
+    // console.log(query)  
+    const {nome, idade} = request.query
+    console.log(nome, idade)
     response.status(200).json([
         "Pessoa 1",
         "Pessoa 2",
@@ -14,23 +34,32 @@ app.get("/users", (request, response)=>{
     ])
 });
 
-app.post("/users", (request, response)=>(
+app.post("/users", (request, response)=>{
+    // const body = request.body
+    // console.log(body)
+    const {nome, idade} = request.body
     response.status(201).json([
         "Pessoa 1",
         "Pessoa 2",
         "Pessoa 3",
         "Pessoa 4"
     ])
-));
+});
 
-app.put("/users", (request, response)=>(
+app.put("/users/:id/:cpf", (request, response)=>{
+    const params = request.params
+    console.log(params)
+    const id = request.params.id
+    console.log(id)
+    const cpf = request.params.cpf
+    console.log(cpf)
     response.status(200).json([
         "Pessoa 1",
         "Pessoa 10",
         "Pessoa 3",
         "Pessoa 4"
     ])
-));
+});
 
 app.delete("/users", (request, response)=>(
     response.status(204).json([
